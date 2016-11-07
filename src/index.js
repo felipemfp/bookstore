8a77ba0b6a9d166cfbe50c43b128b235d8a80f8c
Felipe Pontes <felipemfpontes@gmail.com>
2016-11-07 18:06:24 -0300

    Add components and routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.css'
 
 ReactDOM.render(
-  <App />,
+  <Router routes={routes} history={browserHistory} />,
   document.getElementById('root')
 );
